81485f9065fb64a5c8ca8fac5f88bb82ca6f22ab
Matt Hitches <matt@Matts-MacBook-Air.local>
2026-06-17 18:29:29 +0100

    Responsive styling

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -12,18 +12,20 @@ export default function Hero({
   return (
     <section id="hero">
       <div className="wrap">
-        <div className="hero__content">
-          <h1>{headline}</h1>
-          <p>{intro}</p>
-          <a href="#" className="c-button c-button--blue">Find out more</a>
-        </div>
+        <div className="hero__inner">
+          <div className="hero__content">
+            <h1>{headline}</h1>
+            <p>{intro}</p>
+            <a href="#" className="c-button c-button--blue">Find out more</a>
+          </div>
 
-        <div
-          className="hero__image"
-          style={{
-            backgroundImage: `url(${imageUrl})`,
-          }}
-        ></div>
+          <div
+            className="hero__image"
+            style={{
+              backgroundImage: `url(${imageUrl})`,
+            }}
+          ></div>
+        </div>
       </div>
     </section>
   );
